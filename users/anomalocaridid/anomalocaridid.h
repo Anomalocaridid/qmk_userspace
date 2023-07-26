0a44245cf72454f5433de5d959ce3ca08fce6da4
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2023-07-26 15:55:52 -0400

    userspace: fix key ranges

--- users/anomalocaridid/anomalocaridid.h
+++ users/anomalocaridid/anomalocaridid.h
@@ -7,13 +7,7 @@
 #include QMK_KEYBOARD_H
 
 enum custom_keycodes_user {
-#if defined(KEYBOARD_ergodox_ez)
-    VRSN = EZ_SAFE_RANGE,
-#elif defined(KEYBOARD_ploopyco_trackball_rev1_005)
-    VRSN = PLOOPY_SAFE_RANGE,
-#else
     VRSN = SAFE_RANGE,
-#endif
     FLASH, // Type qmk command for flashing firmware
     KEYMAP_SAFE_RANGE,
 };
