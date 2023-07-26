d7514285ebe4623c3aa9a12ba238a5afd3cc912a
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2023-07-26 18:02:16 -0400

    ergodox ez: move print screen, next rgb fx keys, make fn layer momentary

--- keyboards/ergodox_ez/keymaps/anomalocaridid/keymap.c
+++ keyboards/ergodox_ez/keymaps/anomalocaridid/keymap.c
@@ -19,10 +19,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Base layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |   1  |   2  |   3  |   4  |   5  |      |           |  =   |   6  |   7  |   8  |   9  |   0  |   -    |
+ * |        |   1  |   2  |   3  |   4  |   5  |NextFX|           |  =   |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |   Q  |   W  |   E  |   R  |   T  | FNCT |           |Delete|   Y  |   U  |   I  |   O  |   P  |   \    |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        |   Q  |   W  |   E  |   R  |   T  |FNCT/ |           |Delete|   Y  |   U  |   I  |   O  |   P  |   \    |
+ * |--------+------+------+------+------+------|PrtScr|           |      |------+------+------+------+------+--------|
  * |L Shift(|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |R Shift)|
  * |--------+------+------+------+------+------| Left |           |Right |------+------+------+------+------+--------|
  * | L Ctrl |   Z  |   X  |   C  |   V  |   B  | GUI  |           |GUI   |   N  |   M  |   ,  |   .  |   /  | R Ctrl |
@@ -38,8 +38,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 `--------------------'       `--------------------'
  */
     [BASE] = LAYOUT_ergodox_pretty(
-        XXXXXXX, KC_1,    KC_2,    KC_3,    KC_4,    KC_5, XXXXXXX,                         KC_EQL,  KC_6, KC_7,  KC_8,    KC_9,    KC_0,    KC_MINUS,
-        XXXXXXX, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, TT(FNCT),                        KC_DEL,  KC_Y, KC_U,  KC_I,    KC_O,    KC_P,    KC_BSLS,
+        XXXXXXX, KC_1,    KC_2,    KC_3,    KC_4,    KC_5, RGB_MOD,                         KC_EQL,  KC_6, KC_7,  KC_8,    KC_9,    KC_0,    KC_MINUS,
+        XXXXXXX, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, LT(FNCT, KC_PSCR),               KC_DEL,  KC_Y, KC_U,  KC_I,    KC_O,    KC_P,    KC_BSLS,
         SC_LSPO, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                           KC_H, KC_J,  KC_K,    KC_L,    KC_SCLN, SC_RSPC,
         KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_LGUI,                         KC_RGUI, KC_N, KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL,
         KC_LALT, QK_GESC, KC_QUOT, KC_LEFT, KC_RGHT,                                                       KC_UP, KC_DOWN, KC_LBRC, KC_RBRC, KC_RALT,
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 1: Function layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |Mac1Rec |  F1  |  F2  |  F3  |  F4  |  F5  |NextFX|           |PrtScr|  F6  |  F7  |  F8  |  F9  |  F10 | Reboot |
+ * |Mac1Rec |  F1  |  F2  |  F3  |  F4  |  F5  |NextFX|           |  =   |  F6  |  F7  |  F8  |  F9  |  F10 | Reboot |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |Mac2Rec |  F11 |  F12 |  F13 |  F14 |  F15 | FNCT |           |Delete|  F16 |  F17 |  F18 |  F19 |  F20 | Flash  |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 `--------------------'       `--------------------'
  */
     [FNCT] = LAYOUT_ergodox_pretty(
-        DM_REC1, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   RGB_MOD,                   KC_PSCR, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  QK_BOOT,
+        DM_REC1, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,                   _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  QK_BOOT,
         DM_REC2, KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15,  _______,                   _______, KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  FLASH,
         DM_PLY1, KC_F21,  KC_F22,  KC_F23,  KC_F24,  XXXXXXX,                                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         DM_PLY2, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,                   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, ALTCASE,
@@ -83,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 2: Mouse layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |   1  |   2  |   3  |   4  |   5  |      |           |  =   |   6  |   7  |   8  |   9  |   0  |   -    |
+ * |        |   1  |   2  |   3  |   4  |   5  |NextFX|           |  =   |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |        |   Q  |   W  |   E  |   R  |   T  |      |           |Delete| W Up |W Left| M Up |WRight|   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -115,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 3: Arrow layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |   1  |   2  |   3  |   4  |   5  |      |           |  =   |   6  |   7  |   8  |   9  |   0  |   -    |
+ * |        |   1  |   2  |   3  |   4  |   5  |NextFX|           |  =   |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |        |   Q  |   W  |   E  |   R  |   T  |      |           |Delete|   Y  |   U  |  Up  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
