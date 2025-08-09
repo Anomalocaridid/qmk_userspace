8a84d44b7d91b5e8dfa1560661f8f866736363bb
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2025-08-08 21:44:06 -0400

    refactor: move rgb effect settings to userspace

--- keyboards/adafruit/macropad/keymaps/anomalocaridid/config.h
+++ keyboards/adafruit/macropad/keymaps/anomalocaridid/config.h
@@ -9,10 +9,3 @@
 
 // Disable unused features
 #define NO_ACTION_TAPPING // Disable mod tap, layer tap, etc.
-
-// Disable undesired animations
-#undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
-#undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
-#undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
-#undef ENABLE_RGB_MATRIX_SPLASH
-#undef ENABLE_RGB_MATRIX_SOLID_SPLASH
